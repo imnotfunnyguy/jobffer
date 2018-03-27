9af6f30cebf3b479791a5173b5d23481f26d170c
unknown <imnotfunnyguy@hotmail.com>
2018-03-26 21:17:18 -0400

    updated setup script

--- setup.bat
+++ setup.bat
@@ -1,3 +1,10 @@
 REM this is a automation script. 
 
 echo "This is Jobffer Project. 26032018" 
+
+REM git config --global user.email "imnotfunnyguy@hotmail.com"
+
+REM set USERNAME=$1
+
+git clone https://github.com/imnotfunnyguy/jobffer.git
+
